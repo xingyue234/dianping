b615a8d73f228686470931455d817f3e3d687b18
xingyue234 <1780982620@qq.com>
2017-10-02 21:36:30 +0800

    修改build时请求路径问题

--- front-app/myproject/src/store/actions.js
+++ front-app/myproject/src/store/actions.js
@@ -1,25 +1,27 @@
 import QS from 'qs'; //node里，将对象转换成参数字符串
 
- //通过后端代理，处理跨域，向8080发送请求时，相当于向8888发送请求
+ //通过后端代理，处理跨域，向9999发送请求时，相当于向8888发送请求
+let debug_mode = true; //设置一个变量,开发环境下为true，需要build时再改成false
+let url_prefix = debug_mode ? '' : 'http://localhost:8888';  //否则为上线之后的域名，这里先填写本地服务器
 
 export default{
     register(store,{$http,data}){
       return $http({
         method:'POST',
-        url:'/api/user/reg',
+        url:`${url_prefix}/api/user/reg`,
         data: QS.stringify(data)
       })
     },
     login(store,{$http,data}){
       return $http({
         method:'POST',
-        url:'/api/user/login',
+        url:`${url_prefix}/api/user/login`,
         data: QS.stringify(data)
       })
     },
     logout(store,{$http}){
       return $http(
-        {url: '/api/user/logout'}
+        {url: `${url_prefix}/api/user/logout`}
       )
     },
     //上传头像
@@ -29,14 +31,14 @@ export default{
       console.log(data.avatar);
       return $http({
         method:'POST',
-        url: '/api/user/avatar/upload',
+        url: `${url_prefix}/api/user/avatar/upload`,
         data: fd
       })
     },
     //获取头像
     getuploadAvatar(store,{$http}){
       $http({
-        url: '/api/user/avatar',
+        url: `${url_prefix}/api/user/avatar`,
       }).then((res)=>{
         //console.log(res);
         store.commit('update_avatar',res.data.avatar);
@@ -45,7 +47,7 @@ export default{
     //获取用户资料
     getUserData(store,{$http}){
        return $http({
-          url: '/api/user/profile',
+          url: `${url_prefix}/api/user/profile`,
        }).then((res)=>{
          //console.log(res.data,'userdata');
          store.commit('update_gender',res.data.gender);
@@ -57,7 +59,7 @@ export default{
     editUserData(store,{$http,data}){
         return $http({
           method:'POST',
-          url: '/api/user/profile/edit',
+          url: `${url_prefix}/api/user/profile/edit`,
           data: QS.stringify(data)
         })
     }
